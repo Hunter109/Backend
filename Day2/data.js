bcba57c182711bd6f2f78c56e284b1a68323db0e
utsab wagle <utsavwagle508@gmail.com>
2025-09-06 08:56:04 +0545

    common js export

---
+++ Day2/data.js
@@ -0,0 +1,20 @@
+// export using common js 
+
+// exports.name = "utsav wagle";
+// exports.faculty = "Science and Technology"
+
+// module.exports = name;
+// module.exports = faculty;
+
+let data1 = [
+
+    {
+        name: "utsav wagle"
+    }, {
+        name: "Hunter"
+    }, {
+        name: "4nonymous"
+    }
+]
+
+module.exports = data1;
---
+++ Day2/student.js
@@ -0,0 +1,5 @@
+let student = require('./data.js')
+// console.log(student)
+for (let name of student) {
+    console.log(name.name)
+}
